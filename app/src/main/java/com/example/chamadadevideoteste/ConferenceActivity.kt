eac5a6215fc7194238034bd3ddcb5a2a482eb90d
Luucas Feio <lucascamp30@gmail.com>
2025-11-20 19:16:12 -0300

    comit do git hehehe

--- app/src/main/java/com/example/chamadadevideoteste/ConferenceActivity.kt
+++ app/src/main/java/com/example/chamadadevideoteste/ConferenceActivity.kt
@@ -1,12 +1,24 @@
 package com.example.chamadadevideoteste
 
+import android.R
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class ConferenceActivity : AppCompatActivity() {
+
+    lateinit var meetingID : String
+    lateinit var userName : String
+    lateinit var meetingIDTextView : TextView
+    lateinit var shareBtn : ImageView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,42 @@ class ConferenceActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        meetingIDTextView = findViewById(R.id.meeting_id_textview)
+        shareBtn = findViewById(R.id.share_btn_imageview)
+
+        meetingID = intent.getStringExtra("MEETING_ID").toString()
+        userName = intent.getStringExtra("USERNAME").toString()
+
+        meetingIDTextView.setText("Meeting ID : "+meetingID)
+
+
     }
+
+
+    fun addFragment() {
+        val appID: Long = AppConstants.APP_ID
+        val appSign: String = AppConstants.APP_SIGN
+
+        var conferenceID: String? = meetingID
+        var userID: String? = userName
+        var userName: String? = userName
+
+        val config = ZegoUIKitPrebuiltVideoConferenceConfig()
+        val fragment = ZegoUIKitPrebuiltVideoConferenceFragment.newInstance(
+            appID,
+            appSign,
+            userID,
+            userName,
+            conferenceID // <--- O ID da conferência está AQUI, na posição correta
+            // REMOVA A LINHA 57 QUE TINHA "conference,"
+        )
+
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commitNow()
+    }
+
+
+
 }
